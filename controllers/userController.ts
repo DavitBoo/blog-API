7893b7097c361c3a60c3030d10b804f6f202bd80
DavitBoo <davidmarras@gmail.com>
2023-12-01 18:24:07 +0100

    creating user error handling

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,17 +1,29 @@
 import express from "express";
 import { User } from "../models/user";
 
-
 const router = express.Router();
 
 // Create user route
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
-  const newUser = new User({ username, password });
-  await newUser.save();
+  try {
+    // Check if user already exists
+    const userExists = await User.findOne({ username });
 
-  res.status(201).send({ message: "User created successfully" });
+    if (userExists) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Create a new user
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // Login user route
@@ -19,6 +31,7 @@ router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   const user = await User.findOne({ username });
+
 });
 
 router.get("/users", async (req, res) => {
