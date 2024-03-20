c6a0af475d16bfe5951c0fd7aa04f5b21fed2053
DavitBoo <davidmarras@gmail.com>
2024-03-20 22:42:22 +0100

    Comment controller change

--- controllers/commentController.ts
+++ controllers/commentController.ts
@@ -5,11 +5,11 @@ const router = express.Router();
 
 
 // Create comment route
-router.post('/posts/:id/comments/:postId', async (req, res) => {
+router.post('/posts/:id/comments', async (req, res) => {
   const { username, email, commentContent } = req.body;
-  const postId = req.params.postId;
+  // const postId = req.params.id;
 
-  const newComment = new Comment({ username, email, commentContent, postId });
+  const newComment = new Comment({ username, email, commentContent /*, postId*/ });
   await newComment.save();
 
   res.status(201).send({ message: 'Comment created successfully' });
