c4b3f45f788bbf65f34ffcaf97cbbe4207dd9593
DavitBoo <davidmarras@gmail.com>
2024-05-13 20:45:39 +0200

    cleaning code idea about logout from server

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -26,7 +26,7 @@ router.post("/register", async (req, res) => {
     const hash = bcrypt.hashSync(password, salt);
     // console.log(hash);
 
-    // Create a new user 
+    // Create a new user
     const newUser = new User({ username, password: hash });
     await newUser.save();
 
@@ -41,52 +41,31 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   // const { username, password } = {username:'David', password:'password'};
   const { username, password } = req.body;
-  
+
   const user = await User.findOne({ username });
-  jwt.sign({user}, 'secretkey', {expiresIn: 30}, (err: Error | null, token?: string) => {
+  jwt.sign({ user }, "secretkey", { expiresIn: 30 }, (err: Error | null, token?: string) => {
     res.json({
-      token
-    })
-  } );
-  
+      token,
+    });
+  });
 });
 
-
-// // Login user route
-// router.post("/login", async (req, res) => {
-//   const { username, password } = req.body;
-  
-//   const user: IUser | null = await User.findOne({ username });
-
-//   if (!user) {
-//     return res.status(404).json({ message: "User not found" });
-//   }
-
-//   // Sign the token with a specific callback
-//   const signCallback: SignCallback = (err, token) => {
-//     if (err) {
-//       return res.status(500).json({ message: "Error signing token" });
-//     }
-
-//     const nonNullableToken: string = token || '';
-
-//     // Use the JwtToken type for the token response
-//     const tokenResponse: JwtToken = { token: nonNullableToken };
-
-//     res.json(tokenResponse);
-//   };
-
-//   // Use the JwtPayload type for the payload
-//   jwt.sign({ user } as JwtPayload, 'secretkey', signCallback);
-// });
-
+/*
+router.post("/logout", async (req, res) => {
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .json({ message: "Logout successful" });
+});
+*/
 
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
   } catch (error) {
-
     console.error("Error al obtener usuarios:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
