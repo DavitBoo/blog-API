924ac1ad44acfaac8338186805da3022a83d11ea
DavitBoo <davidmarras@gmail.com>
2024-07-15 19:51:27 +0200

    some adjustment for better debug for token verification

--- middleware/verifyToken.ts
+++ middleware/verifyToken.ts
@@ -1,34 +1,33 @@
 import express, { NextFunction, Request, Response } from "express";
-import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+import jwt, { Secret, JwtPayload } from "jsonwebtoken";
 
 interface CustomRequest extends Request {
-    token: string | JwtPayload;
-   }
+  token: string | JwtPayload;
+}
 
-//format of token 
+//format of token
 //Authorization: Bearer <access_token>
 
 // verify token
-function verifyToken(req: Request, res: Response, next: NextFunction){
-    //get auth header value
-    const bearerHeader = req.headers['authorization'];
-    console.log(bearerHeader);
-  
-    // Check if bearer is undefined
-    if(bearerHeader !== undefined){
-      // split at the space
-      const bearer = bearerHeader.split(' ');
-      const bearerToken = bearer[1];
-      //set the token 
-      (req as CustomRequest).token = bearerToken;
-      
-      next();
-  
-    }else {
-      //forbidden
-      console.log('test :(');
-      res.sendStatus(403)
-    }
-  }
+function verifyToken(req: Request, res: Response, next: NextFunction) {
+  //get auth header value
+  const bearerHeader = req.headers["authorization"];
+  console.log(bearerHeader);
 
-  export default verifyToken
+  // Check if bearer is undefined
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+
+    if (!bearerToken) {
+      return res.status(403).send({ error: "Token not provided" });
+    }
+
+    (req as CustomRequest).token = bearerToken;
+    next();
+  } else {
+    return res.status(403).send({ error: "Forbidden" });
+  }
+}
+
+export default verifyToken;
